74a18c47608c686bc6bebf40572c295be33b4789
Matt Jibson <matt.jibson@gmail.com>
2019-12-06 14:18:21 -0700

    goog analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ import {
 	useLocation,
 	useHistory,
 } from 'react-router-dom';
+import GAListener from './tracker';
 
 const flexChildrenClass = 'bg-dp01 pa1 ma1';
 
@@ -499,31 +500,36 @@ function About() {
 }
 
 export default function App() {
+	const trackingId =
+		process.env.NODE_ENV === 'production' ? 'UA-154150569-1' : undefined;
+
 	return (
 		<Router>
-			<div className="sans-serif">
-				<nav className="pa3 bg-dp04">
-					<ul className="list ma0 pa0">
-						<li className="ma2">
-							<Link to="/">fittin.gs</Link>
-						</li>
-						<li className="ma2">
-							<Link to="/search">search</Link>
-						</li>
-						<li className="ma2">
-							<Link to="/about">about</Link>
-						</li>
-					</ul>
-				</nav>
-				<div className="ma1">
-					<Switch>
-						<Route path="/fit/:id" children={<Fit />} />
-						<Route path="/search" children={<Search />} />
-						<Route path="/about" children={<About />} />
-						<Route path="/" children={<Fits />} />
-					</Switch>
+			<GAListener trackingId={trackingId}>
+				<div className="sans-serif">
+					<nav className="pa3 bg-dp04">
+						<ul className="list ma0 pa0">
+							<li className="ma2">
+								<Link to="/">fittin.gs</Link>
+							</li>
+							<li className="ma2">
+								<Link to="/search">search</Link>
+							</li>
+							<li className="ma2">
+								<Link to="/about">about</Link>
+							</li>
+						</ul>
+					</nav>
+					<div className="ma1">
+						<Switch>
+							<Route path="/fit/:id" children={<Fit />} />
+							<Route path="/search" children={<Search />} />
+							<Route path="/about" children={<About />} />
+							<Route path="/" children={<Fits />} />
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</GAListener>
 		</Router>
 	);
 }
