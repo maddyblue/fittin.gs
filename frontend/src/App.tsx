b990397630dd623bcd788358afbdebb853ebdd58
Matt Jibson <matt.jibson@gmail.com>
2019-12-14 10:19:02 -0700

    add an error boundary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -41,16 +41,39 @@ export default function App() {
 						</ul>
 					</nav>
 					<div className="ma1">
-						<Switch>
-							<Route path="/fit/:id" children={<Fit />} />
-							<Route path="/search" children={<Search />} />
-							<Route path="/about" children={<About />} />
-							<Route path="/saved" children={<Saved />} />
-							<Route path="/" children={<Fits />} />
-						</Switch>
+						<ErrorBoundary>
+							<Switch>
+								<Route path="/fit/:id" children={<Fit />} />
+								<Route path="/search" children={<Search />} />
+								<Route path="/about" children={<About />} />
+								<Route path="/saved" children={<Saved />} />
+								<Route path="/" children={<Fits />} />
+							</Switch>
+						</ErrorBoundary>
 					</div>
 				</div>
 			</GAListener>
 		</Router>
 	);
+}
+
+class ErrorBoundary extends React.Component<any, { hasError: boolean }> {
+	constructor(props: any) {
+		super(props);
+		this.state = { hasError: false };
+	}
+
+	static getDerivedStateFromError(error: any) {
+		// Update state so the next render will show the fallback UI.
+		return { hasError: true };
+	}
+
+	render() {
+		if (this.state.hasError) {
+			// You can render any custom fallback UI
+			return <h1>Something went wrong.</h1>;
+		}
+
+		return this.props.children;
+	}
 }
